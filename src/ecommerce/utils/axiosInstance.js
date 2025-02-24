6647409205193dba7376d0054b24048e95c5d795
EsmailKhaleel <esmailkhaleel27@gmail.com>
2025-02-24 18:10:05 +0200

    dealing-with-deployed-server

--- src/ecommerce/utils/axiosInstance.js
+++ src/ecommerce/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'json-server-project-production.up.railway.app',
+    baseURL: 'https://json-server-project-production.up.railway.app',
     timeout: 1000,
     headers: {
         'Content-Type': 'application/json'
