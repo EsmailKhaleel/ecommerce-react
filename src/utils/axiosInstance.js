36854dbb2915b578e1102903b69cc88cef8a5c41
EsmailKhaleel <esmailkhaleel27@gmail.com>
2025-06-12 03:40:45 +0300

    Fix axios baseURL to include API path for correct endpoint access

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'https://json-server-project-khaki.vercel.app',
+    baseURL: 'https://json-server-project-khaki.vercel.app/api',
     // baseURL: 'http://localhost:3000/api',
     timeout: 2000,
     headers: {
